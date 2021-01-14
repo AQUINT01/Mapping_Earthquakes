571372f279f15f0ab86a1e8c1beb5c9bfbb09bf2
AQUINT01 <63432637+AQUINT01@users.noreply.github.com>
2021-01-14 15:51:25 -0500

    add all files

--- Mapping_GeoJSON_Points/static/js/logic_13_5_3.js
+++ Mapping_GeoJSON_Points/static/js/logic_13_5_3.js
@@ -1,28 +1,25 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
-
-
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-L.circle([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillcolor: '#ffffa1'
- }).addTo(map); 
-
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/AQUINT01/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
